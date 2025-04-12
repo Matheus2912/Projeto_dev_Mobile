4b032d6cc9d2b9868bed021c857b0c50b1f7c834
Matheus <souza09.matheus@gmail.com>
2025-04-12 20:09:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appdereunioes/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.appdereunioes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appdereunioes.MainActivity;
+import com.example.appdereunioes.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail, editSenha;
+    private Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Aqui você pode adicionar alguma validação falsa se quiser
+                String email = editEmail.getText().toString().trim();
+                String senha = editSenha.getText().toString().trim();
+
+                // Redirecionar para tela principal (MainActivity)
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Impede voltar pra tela de login com o botão "voltar"
+            }
+        });
+    }
+}
+
